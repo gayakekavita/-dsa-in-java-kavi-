e9685b4beef67d0949d6b0f12ad67701bb95c523
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-02-17 21:42:54 +0530

    Create 8rotate_array_by_one(optimal solution).java

---
+++ 8rotate_array_by_one(optimal solution).java
@@ -0,0 +1,25 @@
+/* 
+ input:
+ arr=4,5,6,8,9
+ after one space rotate 
+ output:
+ arr=5,6,8,9,4
+*/
+import java.util.* ;
+import java.io.*; 
+
+
+public class Solution {
+
+    static int[] rotateArray(int[] arr, int n) {
+        // Write your code here.
+        int temp=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            arr[i-1]=arr[i];
+        }
+        arr[n-1]=temp;
+        return arr;
+
+    }
+}
