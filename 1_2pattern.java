991cfdda4729abe26192b27b99d47fb4c075e19a
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-02-19 17:28:17 +0530

    Create 1_2pattern.java

---
+++ 1_2pattern.java
@@ -0,0 +1,29 @@
+/*
+   Problem statement
+Sam is making a Triangular painting for a maths project.
+
+An N-dimensional Triangle is represented by the lower triangle of the pattern filled with integers representing the row number.
+
+For every value of ‘N’, help sam to print the corresponding Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+1
+2 2 
+3 3 3
+*/
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=i+1;j++)
+            {
+                System.out.print(i+1 +" ");
+            }
+            System.out.println();
+        }
+    }
+}
