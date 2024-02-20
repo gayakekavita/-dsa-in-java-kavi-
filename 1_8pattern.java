ccc4c4b9e304220d987f39bcf29526bce6c3f51a
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-02-20 19:20:27 +0530

    Create 1_8pattern.java

---
+++ 1_8pattern.java
@@ -0,0 +1,38 @@
+
+/*
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Rotated Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*
+**
+***
+**
+*
+*/
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i+1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
