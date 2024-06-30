269f90a9428b69bfe1f59e34c0e76beabd7e8b61
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-06-30 12:46:06 +0530

    Update 51_valid_palindrome.java

--- 51_valid_palindrome.java
+++ 51_valid_palindrome.java
@@ -23,3 +23,34 @@ Output: true
 Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
 */
+
+/*
+-- here we are converting string into lowercase using toLowerCase() function.
+-- then we are creating a variable using StringBuilder class.
+--  then we are checking for lowercase letters and numeric characters.
+-- at last we are doing operation of checking whether given string is palindrome or not.
+ 
+*/
+
+import java.util.*;
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();  //converting given string into lowercase 
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>=97 && s.charAt(i)<=122 ||  s.charAt(i)>=48 && s.charAt(i)<=57)  //storing only lowercase letters and numbers . 
+            {
+                sb.append(s.charAt(i));
+            }
+        }
+        for(int i=0;i<sb.length()/2;i++)
+        {
+            if(sb.charAt(i) != sb.charAt(sb.length()-1-i))
+            {
+                return false ;
+            }
+        }
+        return true;
+    }
+}
