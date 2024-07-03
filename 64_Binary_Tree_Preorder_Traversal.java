ad8a5e5f66624cb31c12bd00b027e70cccc5733e
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-07-03 12:08:23 +0530

    Create 64_Binary_Tree_Preorder_Traversal.java

---
+++ 64_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,19 @@
+-- if we have hierarchical data then we use tree data structure.
+--when a node that do not have parent node is called as "root node" .
+  # The root node is the topmost node in a tree data structure.
+  # serving as the starting point from which all other nodes descend.
+  # It has no parent and is the only node without a predecessor
+  # In binary tree root node have two childes :
+                             1.left node (left child)
+                             2. right node (right child)
+  # in Binary tree parent node have atmost "two" children not more than "two children" it can be less than "two" but not more than "two".
+-- Preorder 
+         types of traversal :
+                1. Breadth first
+                      # here we traverse tree horizontally.
+                      # it is also known as "level" traversal.
+                2. Depth First 
+                      # here we traverse tree vertically .
+                      # from top to bottom or vice versa .
+                      # types of depth first tra
+  
