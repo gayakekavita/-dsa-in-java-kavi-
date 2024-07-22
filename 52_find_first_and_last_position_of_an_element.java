457bbac4927fa0161736731598ed68b8b0753795
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-07-22 22:11:14 +0530

    Update 52_find_first_and_last_position_of_an_element.java

--- 52_find_first_and_last_position_of_an_element.java
+++ 52_find_first_and_last_position_of_an_element.java
@@ -149,6 +149,53 @@ The while loop terminates since low is not less than high.
 Set res[1] = high = 4
 So the final result is res = [3, 4]
            
-         
+
+
+
+    //solution easy ::
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans={-1,-1};
+        int start=search(nums,target,true);
+        int end= search(nums,target,false);
+        ans[0]=start;
+        ans[1]=end;
+        return ans;        
+    }
+    public static int search(int[] nums,int target, boolean startindex)
+    {
+        int ans=-1;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high)
+        {
+            int mid= low+(high-low)/2;
+            if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else if( nums[mid]< target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                 ans=mid;
+                 if(startindex)
+                 {
+                    high=mid-1;
+                   
+                 }
+                 else
+                 {
+                     low=mid+1;
+                 }
+
+            }
+            
+        }
+        return ans;
+    }
+}
          
   
